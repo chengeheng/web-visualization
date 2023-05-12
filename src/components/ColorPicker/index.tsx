bfd702d03870667ce7780c158e51adf4ab8305b1
chengeheng <chengeheng@cowarobot.com>
2023-05-12 14:55:08 +0800

    feat: 完善颜色渐变计算的逻辑

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,19 +1,65 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
 
-import { drawBg } from "./util";
+import { drawBg, drawCircle, calcuateColor } from "./util";
 
 import styles from "./index.module.scss";
 
 const ColorPicker: FC = () => {
     const ref = useRef<HTMLCanvasElement | null>(null);
-    const [color] = useState<string>("");
-    const [type] = useState<string>("HEX");
+    const [color, setColor] = useState<string>("#ff0000");
+    const [position, setPosition] = useState<[number, number]>([0, 0]);
+    // const [selectColor, setSelectColor] = useState<string>("#ff0000");
+    // const [type] = useState<string>("HEX");
+
+    const selectColor = useMemo<string>(() => {
+        if (ref.current) {
+            const { width, height } = ref.current;
+            const [x, y] = position;
+            return calcuateColor(x, y, width, height, color);
+        } else {
+            return "#FFFFFF";
+        }
+    }, [ref, position, color]);
 
     useEffect(() => {
+        // 根据选中颜色绘制背景
         if (ref.current) {
             const canvas = ref.current;
             const ctx = canvas.getContext("2d")!;
-            drawBg(ctx, "#ff0000");
+            drawBg(ctx, color);
+            drawCircle(ctx, ...position);
+        }
+    }, [ref, color, position]);
+
+    useEffect(() => {
+        if (ref.current) {
+            // 添加事件
+            const canvas = ref.current;
+            const { left, top } = canvas.getBoundingClientRect();
+            const [width, height] = [canvas.width, canvas.height];
+            const onMouseMove = (e: MouseEvent) => {
+                const [x, y] = [e.clientX, e.clientY];
+                const finalX = x < left ? 0 : x > left + width ? width : x - left;
+                const finalY = y < top ? 0 : y > top + height ? height : y - top;
+                setPosition([finalX, finalY]);
+            };
+            const addMouseUpFn = () => {
+                document.removeEventListener("mousemove", onMouseMove);
+            };
+            canvas.onmousedown = (e) => {
+                const [x, y] = [e.offsetX, e.offsetY];
+                setPosition([x, y]);
+                document.addEventListener("mousemove", onMouseMove);
+            };
+            canvas.onmouseup = (e) => {
+                document.removeEventListener("mousemove", onMouseMove);
+            };
+            document.addEventListener("mouseup", addMouseUpFn);
+            return () => {
+                canvas.onmousedown = null;
+                canvas.onmouseup = null;
+                document.removeEventListener("mouseup", addMouseUpFn);
+            };
         }
     }, [ref]);
 
@@ -21,15 +67,13 @@ const ColorPicker: FC = () => {
         <div className={styles.main}>
             <div className={styles.left}></div>
             <div className={styles.right}>
-                <canvas className={styles.canvas} ref={ref} width={300} height={300}></canvas>
-                <div className={styles.color}>
-                    <div className={styles.des}>
-                        <div className={styles.text} style={{ backgroundColor: color ?? "#fff" }}>
-                            {color}
-                        </div>
-                        <div className={styles.label}>{type}</div>
-                    </div>
-                    <div className={styles.change}></div>
+                <div className={styles.top}>
+                    <div className={styles.color} style={{ backgroundColor: selectColor ?? "#fff" }}></div>
+                    <div className={styles.text}>{selectColor}</div>
+                </div>
+                <div className={styles.bottom}>
+                    <canvas className={styles.canvas} ref={ref} width={300} height={200}></canvas>
+                    <div className={styles.picker}></div>
                 </div>
             </div>
         </div>
--- src/components/ColorPicker/util.ts
+++ src/components/ColorPicker/util.ts
@@ -17,6 +17,51 @@ const drawBg: (ctx: CanvasRenderingContext2D, color: string) => void = (ctx, col
     ctx.fillRect(0, 0, width, height);
     ctx.fillStyle = directionGradient;
     ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+};
+const drawCircle: (ctx: CanvasRenderingContext2D, x: number, y: number) => void = (ctx, x, y) => {
+    ctx.save();
+    ctx.beginPath();
+    const raidalGradient = ctx.createRadialGradient(x, y, 3, x, y, 4);
+    raidalGradient.addColorStop(0, "#333");
+    raidalGradient.addColorStop(1, "#fff");
+    ctx.strokeStyle = raidalGradient;
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
+    ctx.arc(x, y, 4, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.closePath();
+    ctx.restore();
 };
 
-export { drawBg };
+const calcuateColor: (x: number, y: number, width: number, height: number, color: string) => string = (
+    x,
+    y,
+    width,
+    height,
+    color
+) => {
+    const opacityOne = (width - x) / width;
+    const opacityTwo = y / height;
+    const [r, g, b] = hue2rgb(color);
+    return rgb2hue(
+        r * (1 - opacityOne) * (1 - opacityTwo) + 255 * opacityOne * (1 - opacityTwo),
+        g * (1 - opacityOne) * (1 - opacityTwo) + 255 * opacityOne * (1 - opacityTwo),
+        b * (1 - opacityOne) * (1 - opacityTwo) + 255 * opacityOne * (1 - opacityTwo)
+    );
+};
+
+const rgb2hue: (r: number, g: number, b: number) => string = (r, g, b) => {
+    const hex = "#" + ((1 << 24) + (Math.round(r) << 16) + (Math.round(g) << 8) + Math.round(b)).toString(16).slice(1);
+    return hex;
+};
+
+const hue2rgb: (color: string) => [number, number, number] = (color) => {
+    var a, b, c;
+    a = color.slice(1, 3);
+    b = color.slice(3, 5);
+    c = color.slice(5, 7);
+    return [parseInt(a, 16), parseInt(b, 16), parseInt(c, 16)];
+};
+
+export { drawBg, drawCircle, rgb2hue, calcuateColor, hue2rgb };
